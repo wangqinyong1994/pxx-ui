b32283561a465f5023cf2e4db69f0d3670598af0
wangqinyong1994 <wqy1994yeah@163.com>
2020-10-23 16:47:22 +0800

    :pencil: 修改kitchen默认端口

--- site/kitchen/src/template/KitchenSink/index.jsx
+++ site/kitchen/src/template/KitchenSink/index.jsx
@@ -127,7 +127,7 @@ export default class App extends React.Component {
       });
 
     let rootPath = '/kitchen-sink/components';
-    if (window.location.port === '8002') {
+    if (window.location.port === '8002' || window.location.port === '30997') {
       rootPath = '/components';
     }
     return (
